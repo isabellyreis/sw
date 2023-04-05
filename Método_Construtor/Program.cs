03b07539f8a51c382e96e74a0e603843d082c27e
isabellyreis <100379680+isabellyreis@users.noreply.github.com>
2023-04-05 14:37:28 -0300

    Add files via upload

---
+++ Método_Construtor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Método_Construtor;
+class Program
+{
+   static void Main(string[] args)
+    {
+        //Produto p1 = new Produto(); (construtor 1)
+        //Produto p1 = new Produto("SACI PERERE"); (construtor 2)
+        
+        //(construtor 3)
+        Produto p1 = new Produto("ABC",10);
+
+        
+
+        p1.MostraDados();
+    }
+}
